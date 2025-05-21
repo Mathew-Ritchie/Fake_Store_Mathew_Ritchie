a0968613bc5424e80b6890c3cbe37faca1e03ee7
Mathew Ritchie <ritchmat89@gmail.com>
2025-05-21 09:22:14 +0200

    file changes
    removed store items from App.jsx and made a new StoreItems.jsx component. Also made Header.jsx which will become a nav/sorting/filter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
-import React, { useState, useEffect } from "react";
-import useGlobalStore from "./GlobalStore/useGlobalStore";
-import "./App.css";
-import { FaStar } from "react-icons/fa";
+import React from "react";
+import StoreItems from "./Components/StoreItems";
+import Header from "./Components/Header";
 
-export default function FakeStore() {
-  const { storeItems, fetchStoreData, loading, error } = useGlobalStore();
-
-  useEffect(() => {
-    fetchStoreData();
-  }, [fetchStoreData]);
-
-  const truncateText = (text, limit) => {
-    if (text.length <= limit) {
-      return text;
-    }
-    return `${text.slice(0, limit)}...`;
-  };
-
-  console.log(storeItems);
+export default function App() {
   return (
-    <div className="product-container">
-      {loading && <h1>Items loading...</h1>}
-      {error && <h1>There was an error loading data, please try again.</h1>}
-      {storeItems &&
-        storeItems.map((item) => (
-          <div key={item.id} className="product-card">
-            <div className="product-img-div">
-              <img className="product-img" src={item.image} />
-            </div>
-            <h1 className="product-title">{truncateText(item.title, 50)}</h1>
-            <p>From R {item.price}</p>
-            <div className="product-rating-div">
-              <FaStar />
-              <p>{`${item.rating.rate}(${item.rating.count})`}</p>
-            </div>
-          </div>
-        ))}
+    <div>
+      <Header />
+      <StoreItems />
     </div>
   );
 }
---
+++ src/Components/StoreItems.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import useGlobalStore from "../GlobalStore/useGlobalStore";
+import "./storeItems.css";
+import { FaStar } from "react-icons/fa";
+
+export default function StoreItems() {
+  const { storeItems, fetchStoreData, loading, error } = useGlobalStore();
+
+  useEffect(() => {
+    fetchStoreData();
+  }, [fetchStoreData]);
+
+  const truncateText = (text, limit) => {
+    if (text.length <= limit) {
+      return text;
+    }
+    return `${text.slice(0, limit)}...`;
+  };
+
+  console.log(storeItems);
+  return (
+    <div className="product-container">
+      {loading && <h1>Items loading...</h1>}
+      {error && <h1>There was an error loading data, please try again.</h1>}
+      {storeItems &&
+        storeItems.map((item) => (
+          <div key={item.id} className="product-card">
+            <div className="product-img-div">
+              <img className="product-img" src={item.image} />
+            </div>
+            <h1 className="product-title">{truncateText(item.title, 50)}</h1>
+            <p>R {item.price}</p>
+            <div className="product-rating-div">
+              <FaStar />
+              <p>{`${item.rating.rate}(${item.rating.count})`}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
